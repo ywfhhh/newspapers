69236eb7481ea32c54ae943c3e578d4f4a049558
jiahui.xu <jiahui.xu@anker-in.com>
2025-08-27 04:27:51 +0000

    Site updated: 2025-08-27 04:27:50

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// 定义 JavaScript 脚本
+function download(url) {
+    // 检查 URL 是否有效
+    if (!url) {
+        console.error('下载 URL 无效');
+        return;
+    }
+    
+    // 创建一个隐藏隐藏的 <a> 元素用于下载
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = url.split('/').pop(); // 设置下载的文件名（默认为 URL 的最后一部分）
+    document.body.appendChild(a); // 添加到 DOM
+    a.click(); // 触发点击事件
+    document.body.removeChild(a); // 下载完成后移除 <a> 元素
+    }
+    
+    
+    
